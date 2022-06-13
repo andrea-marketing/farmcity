01a10ff53ba0dc90e79ba5e54783acff92e9f321
Adele Albrespy <a.albrespy@gmail.com>
2022-06-13 17:20:40 +0200

    controller et view index show

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,2 +1,22 @@
 class ProducersController < ApplicationController
+  before_action :set_producer, only: %i[show]
+
+  def index
+    @producers = policy_scope(Producer).order(created_at: :desc)
+  end
+
+  def show
+    @producer = Producer.new
+    authorize @producer
+  end
+
+  private
+
+  def set_producer
+    @producer = Producer.find(params[:id])
+  end
+
+  def producer_params
+    params.require(:producer).permit(:producer_type, :name, :address, :description, :contact, :user_id, :photo)
+  end
 end
