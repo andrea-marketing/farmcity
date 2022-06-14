7ded728f4e3fba170ed0bdd67425468be63906ed
Maxim Bernard <maxim.bernard@kedgebs.com>
2022-06-14 16:12:53 +0200

    index front and nav/footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_13_134702) do
+ActiveRecord::Schema.define(version: 2022_06_13_140852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
