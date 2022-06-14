11a53ea0d981a4a49f9a7239d9ced0dfed43281d
judith karouby <judithkarouby@gmail.com>
2022-06-14 16:19:41 +0200

    reviewsss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_13_142607) do
+ActiveRecord::Schema.define(version: 2022_06_14_140217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,16 @@ ActiveRecord::Schema.define(version: 2022_06_13_142607) do
     t.index ["user_id"], name: "index_producers_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "producer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["producer_id"], name: "index_reviews_on_producer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,4 +82,6 @@ ActiveRecord::Schema.define(version: 2022_06_13_142607) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "producers", "users"
+  add_foreign_key "reviews", "producers"
+  add_foreign_key "reviews", "users"
 end
