03fa8f3f9a359c33d8503e55459054f3d9816ba3
judith karouby <judithkarouby@gmail.com>
2022-06-14 17:50:05 +0200

    ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,9 @@
 class Review < ApplicationRecord
+  AUTHORIZED_RATINGS = (0..5)
+
   belongs_to :producer
   belongs_to :user
+
   validates :content, presence: true
+  validates :rating, inclusion: { in: AUTHORIZED_RATINGS }
 end
