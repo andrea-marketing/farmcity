fb245ddf5821d0f6f70b2eed1cec398252826582
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-06-17 10:16:25 +0200

    errors

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,13 +3,3 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-
-<<<<<<< HEAD
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
-
-window.Stimulus = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
-=======
->>>>>>> b8d06495217c273f76e1f4a25aa26196ca56cedc
