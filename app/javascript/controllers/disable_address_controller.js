e02035de96f846e51d82b77c26f155dd603d14eb
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-06-22 16:52:42 +0200

    searchbar with autocomplete

--- app/javascript/controllers/disable_address_controller.js
+++ app/javascript/controllers/disable_address_controller.js
@@ -3,7 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["input", "button"]
 
+connect() {
+  console.log("max dummy")
+}
   disable() {
     this.inputTarget.classList.toggle("d-none")
+    this.buttonTarget.classList.toggle("d-none")
   }
 }
--- app/javascript/controllers/search_producers_controller.js
+++ app/javascript/controllers/search_producers_controller.js
@@ -3,15 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["form", "input", "producers", "markets"]
 
-  connect() {
-    console.log(this.inputTarget)
-    console.log(this.formTarget)
-    console.log(this.cardsTarget)
-  }
 
   update(event) {
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-    console.log(url)
     fetch(url , { headers: { "Accept": "application/json" } })
       .then(response => response.json())
       .then((data) => {
