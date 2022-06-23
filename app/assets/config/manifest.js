cda4502989a61a557f8e45b9993b54d4f8bf9905
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-06-23 11:27:39 +0200

    creating app

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,22 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+{
+  "background_color": "white",
+  "description": "Farmcity will help you improve your grocery-shopping experience by discovering new local markets and producers in your city.",
+  "display": "fullscreen",
+  "name": "Farmcity",
+  "icons": [
+    <% files = Dir.entries(Rails.root.join("app/assets/images/icons/")).select {|f| !File.directory? f} %>
+    <% files.each_with_index do |file, index| %>
+      <% match = file.match(/.+-(?<size>\d{2,3}x\d{2,3}).png/) %>
+      {
+        "src": "<%= image_path "icons/#{file}" %>",
+        "sizes": "<%= match && match[:size] %>",
+        "type": "image/png"
+      }<%= "," unless (files.size - 1) == index %>
+    <% end %>
+  ]
+}
+
+//= link manifest.json
